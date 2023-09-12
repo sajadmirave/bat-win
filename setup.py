607d3ad4318f2d080ff487a280370063543c29e2
sajjad-mirave <banditofsocorstars@gmail.com>
2023-09-12 18:03:09 +0330

    fix issue: can't run globaly

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ from src.commands.Cat.root import root_path as root_path_cat
 from src.commands.Touch.root import root_path as root_path_touch
 
 # set all commands
-os.system(f'doskey bat-ls={root_path_ls}\index.py') #bat-ls
-os.system(f'doskey bat-cat={root_path_cat}\index.py') #bat-cat
-os.system(f'doskey bat-touch={root_path_touch}\index.py') #bat-touch
+os.system(f'doskey bat-ls={root_path_ls}\index.bat') #bat-ls
+os.system(f'doskey bat-cat={root_path_cat}\index.bat') #bat-cat
+os.system(f'doskey bat-touch={root_path_touch}\index.bat') #bat-touch
