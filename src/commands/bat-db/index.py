27c67e527e1c592e66bccb7a7e8d1648218da295
sajjad-mirave <banditofsocorstars@gmail.com>
2023-09-17 20:36:19 +0330

    bat-db and simple structure

--- src/commands/bat-db/index.py
+++ src/commands/bat-db/index.py
@@ -1,22 +1,57 @@
 # save data in file
 import sys
+import os
+
+
+# Add the parent directory (commands/Ls) and the project directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+from storage import root
 
 # bat-db -C collection_name -R read data
 # bat-db -add data -uname my_unique_name
 #  0        1   2   3       4
 args = sys.argv
 
-if args[1] == "-add": 
+def write():
     data = args[2] #data
     uname = args[4] #data
 
     with open(f"src/storage/{uname}.txt","w") as file:
         file.write(data)
 
-# bat-db -read uname
-if args[1] == "-read":
+def read():
     uname = args[2]
 
     with open(f"src/storage/{uname}.txt",'r') as file:
         data = file.readline()
         print(data)
+
+def dbs():
+    storage_path = root.root_path
+    dbs = os.listdir(storage_path)
+
+    for i in dbs:
+        if ".txt" in i:
+            # print(i)
+            x = i.replace('.txt','')
+            print(x)
+
+def help():
+    print("""
+    add: --add content --uname unique_name
+    read: --uname unique_name
+    show all db: --dbs
+    """)
+
+if args[1] == "--add": write()
+
+# bat-db -read uname
+if args[1] == "--read": read()
+
+if args[1] == '--dbs': dbs()
+
+if args[1] == '--help': help()
+
+else: print('invalid command! | type bat-db --help')
---
+++ src/storage/root.py
@@ -0,0 +1,5 @@
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)
+root_path = os.getcwd() 
